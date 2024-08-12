b31a8b9d430ce0359d8f8417b0bc08758241067b
GuiEspim18 <guilherme.monteiro.espim@gmail.com>
2024-08-12 10:37:51 -0300

    example

---
+++ src/main/java/com/mycompany/exemplo/Exemplo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exemplo;
+
+import View.Splash_GUI;
+
+/**
+ *
+ * @author labsfiap
+ */
+public class Exemplo {
+
+    public static void main(String[] args) {
+        new Splash_GUI().setVisible(true);
+    }
+}
